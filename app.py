27dc73fbefc7e0b0c0c2459a54c2832224ee9488
X0gar <micheler.andreas@yahoo.de>
2026-08-28 20:34:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 from streamlit_local_storage import LocalStorage
 
 st.title("⏰ Hell Clock – Relic Completionist Tool")
-
+st.set_page_config(page_title="Hell Clock Tracker", layout="wide")
 # --- VERBINDUNG ZUM GOOGLE SHEET ---
 sheet_id = "1LnwXHeQUr75nDb2VmbTSOBAP1bzl7x7Qul-PGvdHyLU"
 csv_url = "https://docs.google.com/spreadsheets/d/1LnwXHeQUr75nDb2VmbTSOBAP1bzl7x7Qul-PGvdHyLU/export?format=csv&gid=1242009671#gid=1242009671"
